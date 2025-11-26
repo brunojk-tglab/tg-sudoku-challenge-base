cbb7521628b32feba283c0ef3aad99f9698302c6
brunojk-tglab <bruno.jorge.kosloski@tglab.com>
2025-11-26 11:39:22 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+    
     public static void main(String[] args) {
+        
         System.out.println("Start Here");
+
+        // Escolhe uma matriz solucionada para usar no VALIDADOR (VALIDATOR)
+        Integer[][][] solvedMatrix = SudokuSource.getSolvedMatrix(1);
+
+        // ....
+
+        // Escolhe uma matriz NÃO resolvida para usar no SOLUCIONADOR (SOLVER)
+        Integer[][][] unsolvedMatrix = SudokuSource.getUnsolvedMatrix(1);
+        
+        // ....
+
     }
+
 }
